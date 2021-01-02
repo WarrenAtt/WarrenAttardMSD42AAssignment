bb486e3cbc2b69e4b5f6840b2b794e3211765773
WarrenAttard <warren.attard.e22454@mcast.edu.mt>
2021-01-02 11:09:18 +0100

    Added Assets into Hierarchy, Applied Scrolling Background

---
+++ 2D Car Game/Assets/Scripts/BackgroundScroller.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundScroller : MonoBehaviour
+{
+    [SerializeField] float backgroundScrollSpeed = 0.4f;
+
+    // The Material from the texture
+    Material myMaterial;
+
+    // Movement offset
+    Vector2 offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //get the Material of the background from Renderer component
+        myMaterial = GetComponent<Renderer>().material;
+
+        //move in the y-axis at the given speed
+        offset = new Vector2(0f, backgroundScrollSpeed);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //move the texture of the Material by the offset every frame
+        myMaterial.mainTextureOffset += offset * Time.deltaTime;
+    }
+}
