eac575657ab29fb90c7446d3bd9d6986d93d133c
WarrenAttard <warren.attard.e22454@mcast.edu.mt>
2021-01-02 13:15:38 +0100

    Created 5 different Paths, and EnemyPathing() script

---
+++ 2D Car Game/Assets/Scripts/EnemyPathing.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPathing : MonoBehaviour
+{
+    [SerializeField] List<Transform> waypoints;
+    [SerializeField] float enemyMoveSpeed = 2f;
+
+    //saves the waypoint in which we want to go
+    int waypointIndex = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //set the start position of Enemy to the first waypoint.
+        transform.position = waypoints[waypointIndex].transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        enemyMove();   
+    }
+
+    private void enemyMove()
+    {
+        if (waypointIndex <= waypoints.Count - 1)
+        { 
+            //set the targetPosition to the waypoint where we want to go
+            var targetPosition = waypoints[waypointIndex].transform.position;
+
+            //make sure the z-axis is 0
+            targetPosition.z = 0f;
+
+            var enemyMovement = enemyMoveSpeed * Time.deltaTime;
+
+            //move Enemy from current position to targetPosition, at enemyMovement speed
+            transform.position = Vector2.MoveTowards(transform.position, targetPosition, enemyMovement);
+
+            //if enemy reaches the target position
+            if (transform.position == targetPosition)
+            {
+                waypointIndex++;
+            }
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
