eb6edde03550328c9bbfd60d983156fa4c430492
WarrenAttard <warren.attard.e22454@mcast.edu.mt>
2021-01-02 14:40:30 +0100

    Created EnemySpawner()

---
+++ 2D Car Game/Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    //list of WaveConfigs
+    [SerializeField] List<WaveConfig> waveConfigList;
+
+    //start from Wave 0
+    int startingWave = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //set the current wave to Wave 1 [position 0 in List]
+        var currentWave = waveConfigList[startingWave];
+
+        //Start coroutine that spawns all enimies in currentWave
+        StartCoroutine(SpawnAllEnemiesInWave(currentWave));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //when calling Coroutine, specify which Wave we need to spawn enemies from
+    private IEnumerator SpawnAllEnemiesInWave(WaveConfig waveConfig)
+    {
+        //spawn the enemy from waveConfig at the position specified by waveConfig waypoints
+        Instantiate(waveConfig.GetEnemyPrefab(), waveConfig.GetWaypoints()[0].transform.position, Quaternion.identity);
+        //wait timeBetweenSpawns before spawning another enemy
+        yield return new WaitForSeconds(waveConfig.GetTimeBetweenSpawns());
+    }
+}
