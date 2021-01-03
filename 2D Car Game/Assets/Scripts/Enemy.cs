b49fbc1616f7eaa34fb600752214aaeb9d1499f8
WarrenAttard <warren.attard.e22454@mcast.edu.mt>
2021-01-03 14:43:54 +0100

    Created Shredder(), DamageDealer(), Enemy() scripts
    Enimies can now fire to player.

---
+++ 2D Car Game/Assets/Scripts/Enemy.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] float shotCounter;
+    [SerializeField] float minTimeBetweenShots = 0.2f;
+    [SerializeField] float maxTimeBetweenShots = 3f;
+    [SerializeField] GameObject enemyLaserPrefab;
+    [SerializeField] float enemyLaserSpeed = 0.3f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //generate a random number
+        shotCounter = Random.Range(minTimeBetweenShots, maxTimeBetweenShots);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CountDownAndShoot();
+    }
+
+    private void CountDownAndShoot()
+    {
+        //every frame reduce the amount of time for shot
+        shotCounter -= Time.deltaTime;
+
+        if (shotCounter <= 0f)
+        {
+            EnemyFire();
+            //reset shotCounter
+            shotCounter = Random.Range(minTimeBetweenShots, maxTimeBetweenShots);
+        }
+    }
+
+    private void EnemyFire()
+    {
+        //spawn an enemyLaser at Enemy position
+        GameObject enemyLaser = Instantiate(enemyLaserPrefab, transform.position, Quaternion.identity) as GameObject;
+
+        //shoot laser downwards: -enemyLaserSpeed
+        enemyLaser.GetComponent<Rigidbody2D>().velocity = new Vector2(0, -enemyLaserSpeed);
+    }
+}
