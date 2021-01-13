4bc1d0b12becc22087c95b37a269e644c9ddd8b7
WarrenAttard <warren.attard.e22454@mcast.edu.mt>
2021-01-13 10:15:20 +0100

    StartMenu was created and fully functional

---
+++ 2D Car Game/Assets/Scripts/GameSession.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameSession : MonoBehaviour
+{
+    private void Awake()
+    {
+        SetUpSingleton();
+    }
+
+    private void SetUpSingleton()
+    {
+        int numberOfGameSessions = FindObjectsOfType<GameSession>().Length;
+        if (numberOfGameSessions > 1)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+}
